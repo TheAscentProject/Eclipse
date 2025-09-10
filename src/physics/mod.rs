dcea4eb1c30a512dc9045e2e824f376a8253cb51
Nathan Ryver <tantaouibahaabusiness@gmail.com>
2025-09-10 12:36:43 +0100

    feat: expose frame transforms in physics module

--- src/physics/mod.rs
+++ src/physics/mod.rs
@@ -1,6 +1,8 @@
 pub mod rigid_body;
 pub mod integrator;
+pub mod frames;
 
 pub use rigid_body::{RigidBody, RigidBodyState};
 pub use integrator::{Integrator, RungeKutta4};
 pub use rigid_body::StateDerivative;
+pub use frames::FrameTransforms;
