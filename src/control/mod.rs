29c5325d9d16eb0b1a7a9596ea512dde7500645a
Nathan Ryver <tantaouibahaabusiness@gmail.com>
2025-09-11 21:23:29 +0100

    add autopilot_v2

--- src/control/mod.rs
+++ src/control/mod.rs
@@ -1,6 +1,7 @@
 pub mod pid;
 pub mod flight_mode;
 pub mod autopilot;
+pub mod autopilot_v2;
 pub mod gain_schedule;
 pub mod control_allocation;
 pub mod transition_manager;
@@ -8,6 +9,7 @@ pub mod transition_manager;
 pub use pid::PidController;
 pub use flight_mode::{FlightMode, ControlTarget};
 pub use autopilot::{AutoPilot, ControlOutputs};
+pub use autopilot_v2::AutoPilotV2;
 pub use gain_schedule::{GainSchedule, GainSet, PidGains};
 pub use control_allocation::ControlAllocator;
 pub use transition_manager::TransitionManager;
