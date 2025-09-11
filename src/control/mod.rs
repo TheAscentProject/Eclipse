65b0a33cae3dc090da4a9eef93dc437313c813b1
Nathan Ryver <tantaouibahaabusiness@gmail.com>
2025-09-11 21:16:34 +0100

    update the control module to include the components

--- src/control/mod.rs
+++ src/control/mod.rs
@@ -1,7 +1,13 @@
 pub mod pid;
 pub mod flight_mode;
 pub mod autopilot;
+pub mod gain_schedule;
+pub mod control_allocation;
+pub mod transition_manager;
 
 pub use pid::PidController;
 pub use flight_mode::{FlightMode, ControlTarget};
 pub use autopilot::{AutoPilot, ControlOutputs};
+pub use gain_schedule::{GainSchedule, GainSet, PidGains};
+pub use control_allocation::ControlAllocator;
+pub use transition_manager::TransitionManager;
