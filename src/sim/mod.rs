7f296f6094100d1580461b123812d2262bd3ad27
Nathan Ryver <tantaouibahaabusiness@gmail.com>
2025-09-11 13:36:54 +0100

    chore: expose disturbances

--- src/sim/mod.rs
+++ src/sim/mod.rs
@@ -1,7 +1,9 @@
 pub mod simulator;
 pub mod aircraft;
 pub mod visualization;
+pub mod disturbances;
 
 pub use simulator::Simulator;
 pub use aircraft::Aircraft;
 pub use visualization::Visualization;
+pub use disturbances::DisturbanceModel;
